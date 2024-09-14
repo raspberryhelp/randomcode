66b5d48f7ac8a5e6a55a03f2956aa4e713224bdd
raspberryhelp <agnus1603@gmail.com>
2024-09-14 15:38:07 -0500

    adding somethign

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ st.title("Welcome to titles.")
 st.text("Welcome to text.")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.text("There's really nothing here so far, but I intend to add some stuff")
+st.text("In the meantime, have random facts and charts I guess")
+
+input = st.text_input("Enter something random", "")
+st.write({input})
